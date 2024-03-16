58f85846c9083c1014974d1a23fdc3a86247929b
latuan2710 <109055879+latuan2710@users.noreply.github.com>
2024-03-16 14:12:22 +0700

    ok

---
+++ CODE/CSE483_Lab7/src/test/java/cse483/Cse483Lab7ApplicationTests.java
@@ -0,0 +1,13 @@
+package cse483;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cse483Lab7ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
